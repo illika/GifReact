ad402b5177715a3228445925e927b5fead70c2b4
illika <steve.milan7@gmail.com>
2023-05-31 22:09:57 -0500

    Styles | useSate

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,32 @@
+import { useState } from 'react'
+
 export const GifExpertApp = () => {
-  return (
-    <>
-        <h1>GifExpertApp</h1>
-    </>
-  )
+
+    const [categories, setCategories] = useState(['Dragon Ball', 'Lucky Star']);
+
+    const onAddCategory = () => {
+        setCategories(['Fairy Tail', ...categories]);
+        // CALLBACK
+        // setCategories(cat => [...cat, 'Fairy Tail']);
+    };
+
+    return (
+        <>
+            {/* título */}
+            <h1>GifExpertApp</h1>
+
+            {/* Input */}
+
+            <button onClick={onAddCategory}>Agregar</button>
+            {/* Listado de Gif */}
+            <ol>
+                {
+                    categories.map(category => {
+                        return <li key={category}>{category}</li>;
+                    })
+                }
+            </ol>
+            {/* Gif Item */}
+        </>
+    )
 }
