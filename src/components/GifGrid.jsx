618f70261d9c64997906a37c3f6a8a127277d327
illika <steve.milan7@gmail.com>
2023-06-11 14:56:56 -0500

    className y destructuring props

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { GifItem } from "./GifItem";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
@@ -15,13 +16,17 @@ export const GifGrid = ({ category }) => {
     return (
         <>
             <h2> {category} </h2>
-            <ol>
+            <div className="card-grid">
                 {
-                    images.map(image => (
-                         <li key={image.id}>{image.title}</li>
+
+                    images.map((image) => (
+                        <GifItem
+                            key={image.id}
+                            {...image}
+                        />
                     ))
                 }
-            </ol>
+            </div>
         </>
     )
 }
