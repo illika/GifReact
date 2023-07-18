f153aa0531424a13d8211b9d0325630750c80bf4
illika <steve.milan7@gmail.com>
2023-07-17 22:49:09 -0500

    screen. = debug() | getByRole() | getByText() - toBe() | toBeTruthy()

--- tests/components/GifItem.test.jsx
+++ tests/components/GifItem.test.jsx
@@ -1,11 +1,27 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifItem } from "../../src/components/GifItem";
 
 describe("Pruebas en <GitItem />", () => {
+    const title = "Saitama";
+    const url = "http://one-punch.com/saitama.jpg";
+
     test("Hacer match con el snapshot", () => {
-        const title = "Saitama";
-        const url = "http://one-punch.com/saitama.jpg"
-        const {container} = render(<GifItem title={title} url={url} />);
+        const { container } = render(<GifItem title={title} url={url} />);
         expect(container).toMatchSnapshot();
     });
+
+    test("Mostrar imagen con URL y ALT", () => {
+        render(<GifItem title={title} url={url} />);
+        //screen.debug();
+        //expect(screen.getByRole("img").src).toBe(url);
+        const { src, alt } = screen.getByRole("img");
+        expect(src).toBe(url);
+        expect(alt).toBe(title);
+    });
+
+    test("Mostrar titulo en el componente", () => {
+        render(<GifItem title={title} url={url} />);
+        expect(screen.getByText(title)).toBeTruthy();
+    });
+
 });
