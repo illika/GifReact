213b956aec955d5c14fc8cb30b0c9d2d28d127a6
illika <steve.milan7@gmail.com>
2023-07-21 15:55:15 -0500

    jest.mock() - Hook

--- tests/components/GifGrid.test.jsx
+++ tests/components/GifGrid.test.jsx
@@ -1,20 +1,42 @@
 import { render, screen } from "@testing-library/react";
 import { GifGrid } from "../../src/components/GifGrid";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+jest.mock('../../src/hooks/useFetchGifs');
 
 describe("Pruebas en <GifGrid />", () => {
 
     const category = "One Punch";
 
     test("Mostrar Loading", () => {
-
+        useFetchGifs.mockReturnValue({
+            images: [],
+            isLoading: true
+        });
         render(<GifGrid category={category} />);
-        //screen.debug();
         expect(screen.getByText("Cargando..."));
         expect(screen.getByText(category));
-
     });
 
-    test("", () => {
-
+    test("Mostrar items cuando se cargan imagenes useFetchGifs", () => {
+        const gifs = [
+            {
+                id: "ABC",
+                title: "Saitama",
+                url: "https://localhost/Saitama.jpg"
+            },
+            {
+                id: "ABCD",
+                title: "Goku",
+                url: "https://localhost/goku.png"
+            }
+        ];
+        useFetchGifs.mockReturnValue({
+            images: gifs,
+            isLoading: false
+        });
+        render(<GifGrid category={category} />);
+        //screen.debug();
+        expect(screen.getAllByRole("img").length).toBe(2);
     });
 });
